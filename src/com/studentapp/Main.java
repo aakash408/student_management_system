8b16246c95f02c69c17ca2ce6d49dfe81e39f780
Aakash Gupta <aakash408408@gmail.com>
2025-08-09 03:10:39 +0530

    Add Validation

--- src/com/studentapp/Main.java
+++ src/com/studentapp/Main.java
@@ -5,7 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("*********** Student Management System ***********");
 		System.out.println("*********** Welcome ***********");
-
+		
+		Student s1 = new Student("Aakash", 122, "NULL");
+		System.out.println(s1);
 	}
 
 }
--- src/com/studentapp/Student.java
+++ src/com/studentapp/Student.java
@@ -1,5 +1,6 @@
 package com.studentapp;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Student {
@@ -8,6 +9,46 @@ public class Student {
 	private int age;
 	private String studentId;
 	private List<String> courses;
-	
-	
+
+	public Student(String name, int age, String studentId) {
+		super();
+		if (validateAge(age)) {
+			this.name = name;
+			this.age = age;
+			this.studentId = studentId;
+			courses = new ArrayList<String>(); // Initialization Of Courses !!
+		}
+	}
+
+	public void enrollCourse(String course) {
+		courses.add(course);
+		System.out.println("Student is enrolled to " + course + " sucessfully !!!");
+
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + ", studentId=" + studentId + ", courses=" + courses + "]";
+	}
+
+	public void printStudentInfo() {
+		System.out.println("=========== Student Information ===========");
+		System.out.println("Student Name: " + name);
+		System.out.println("Student Age: " + age);
+		System.out.println("Student ID: " + studentId);
+		System.out.println("Enrolled For: " + courses);
+
+	}
+
+	// Validation Methods
+
+	public boolean validateAge(int age) {
+		if (age >= 19 && age <= 35) {
+			return true;
+		} else {
+			System.err.println("Invalid age !!! Student age needs to be between 19 to 35");
+			return false;
+		}
+
+	}
 }
