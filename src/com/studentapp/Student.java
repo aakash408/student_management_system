328697771cba3a222652677786bb4b3141abe3ce
Aakash Gupta <aakash408408@gmail.com>
2025-08-09 01:37:12 +0530

    Class File Changes

--- src/com/studentapp/Student.java
+++ src/com/studentapp/Student.java
@@ -1,5 +1,13 @@
 package com.studentapp;
 
+import java.util.List;
+
 public class Student {
 
+	private String name;
+	private int age;
+	private String studentId;
+	private List<String> courses;
+	
+	
 }
