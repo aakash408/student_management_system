9533874188b746c0c7a2d837d5a15483121b1e56
Aakash Gupta <aakash408408@gmail.com>
2025-08-16 00:58:31 +0530

    Add Exception

--- src/com/studentapp/Main.java
+++ src/com/studentapp/Main.java
@@ -7,9 +7,17 @@ public class Main {
 		System.out.println("*********** Welcome ***********");
 
 		Student s1 = new Student("Aakash Gupta", 22, "S-21");
-		s1.enrollCourse("PlayWright");
+		s1.enrollCourse("AWS");
 		s1.enrollCourse("Java");
 		System.out.println(s1);
+
+		Student s2 = new Student("Uday", 24, "S-11");
+		s2.enrollCourse("Java");
+		s2.printStudentInfo();
+
+		Student s3 = new Student("Ritu", 26, "S-13");
+		s3.enrollCourse("AWS");
+		s3.printStudentInfo();
 	}
 
 }
